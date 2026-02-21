c31f1f0bda32b80b5d5c78cbd7a731f0648ad2a5
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2026-02-21 14:13:31 +0100

    fix: improve config and document bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2026-02-21',
   modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxtjs/i18n'],
   devtools: { enabled: true },
   typescript: {
+    /**
+     * Might throw and error on running the dev server that looks like this:
+     * ```sh
+     * [uncaughtException] ENOENT: no such file or directory, chmod '[directory-path]'
+     * ```
+     * Ignore it. Only happens on the first run and is caused by the type checking process when no typescript files are present in the project.
+     */
     typeCheck: true,
+  },
+  i18n: {
+    defaultLocale: 'en',
   },
 })
